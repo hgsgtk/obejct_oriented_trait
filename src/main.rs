ca3d491f267c63efe6f65eb8ed2dd82080cbf5a8
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-02 18:07:00 +0900

    implement trait object

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 trait SayHello : std::fmt::Display {
     fn say_hello(&self) {
-        println!("Hello. This is {}.", self);
-    }
-}
-
-trait SayGoodbye : std::fmt::Display {
-    fn say_goodbye(&self) {
-        println!("Goodbye. This is {}.", self);
+        println!("Hello, This is {}.", self);
     }
 }
 
@@ -16,39 +10,18 @@ impl SayHello for str {
         println!("Hi. I'm {}.", self);
     }
 }
-
-fn main() {
-    42.say_hello();
-    "Alice".say_hello();
-}
-
-/* 書き方1
-fn greeting<T: SayHello + SayGoodbye>(x: T) {
-    x.say_hello();
-    x.say_goodbye();
-}
-*/
-
-/* 書き方2
-fn greeting<T><x: T) where T: SayHello + SayGoodbye {
-    x.say_hello();
-    s.say_goodbye();
-}
-*/
-
-/* 書き方3
-fn greeting<T>(x: T) where T: SayHello + SayGoodbye {
-    <T as SayHello>::say_hello(&x);
-    <T as SayGoodbye>::say_goodbye(&x);
-}
-*/
-
-/* 書き方4
 impl<'a, T: ?Sized + SayHello> SayHello for &'a T {
     fn say_hello(&self) {
         <T as SayHello>::say_hello(self);
     }
-}*/
+}
 
-// Box::new(42) as BOX<SayHello>
-
+fn main() {
+    let v = vec![
+        Box::new(42) as Box<SayHello>,
+        Box::new("Alice") as Box<SayHello>,
+    ];
+    for x in &v {
+        x.say_hello();
+    }
+}
